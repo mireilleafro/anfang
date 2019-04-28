de3245ea2edaf169c34d6f7c497c92ff9bf270ef
itbsStefan <itbs@visproxy.net>
2019-04-28 12:33:04 +0200

    Modaler Dialog zum versenden einer Whatsapp Nachricht hinzugefügt

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
+// register modal component
+Vue.component('modal', {
+  template: '#modal-template'
+})
+
+// start app
 var app = new Vue({
   el: '#main',
   data: {
+    showModal: false,
     message: 'Hello Vue!'
   }
 })
+
